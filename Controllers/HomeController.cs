1c222985a4fd4b03690bb44f6f677881863590b6
Muhammad Awais Arif <sp22-bcs-113@cuilahore.edu.pk>
2025-05-29 04:11:36 +0500

    Views Updates, Edit and Delete Functionality added. Charts Added. Contact and About pages constructed.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using StudentMgmtApp.Models;
 
 namespace StudentMgmtApp.Controllers
 {
     public class HomeController : Controller
     {
+        private StudentDbEntities db = new StudentDbEntities();
+
         public ActionResult Index()
         {
-            return View();
+            // Students per Department
+            var deptStats = db.Departments
+                              .Select(d => new {
+                                  Name = d.Name,
+                                  Count = d.Students.Count()
+                              }).ToList();
+
+            // Enrollments per Course
+            var courseStats = db.Courses
+                                .Select(c => new {
+                                    Title = c.Title,
+                                    Count = c.Enrollments.Count()
+                                }).ToList();
+
+            var vm = new DashboardViewModel
+            {
+                DepartmentNames = deptStats.Select(x => x.Name).ToList(),
+                StudentsPerDepartment = deptStats.Select(x => x.Count).ToList(),
+                CourseTitles = courseStats.Select(x => x.Title).ToList(),
+                EnrollmentsPerCourse = courseStats.Select(x => x.Count).ToList()
+            };
+
+            return View(vm);
         }
 
+
+        // GET: Home/About
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "Project details and overview.";
             return View();
         }
 
+        // GET: Home/Contact
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "Your contact details.";
             return View();
         }
     }
---
+++ Models/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Models/DashboardViewModel.cs
+using System.Collections.Generic;
+
+namespace StudentMgmtApp.Models
+{
+    public class DashboardViewModel
+    {
+        // Labels and counts for Chart 1
+        public List<string> DepartmentNames { get; set; }
+        public List<int> StudentsPerDepartment { get; set; }
+
+        // Labels and counts for Chart 2
+        public List<string> CourseTitles { get; set; }
+        public List<int> EnrollmentsPerCourse { get; set; }
+    }
+}
